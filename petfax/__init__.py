925baf849c7c5427b9c66b0c2ee0412b51f256a1
Samuel Kraus <samuelkraus@Samuels-MBP.lan>
2024-04-01 13:16:47 -0400

    completing assignment 5

--- petfax/__init__.py
+++ petfax/__init__.py
@@ -1,13 +1,20 @@
 from flask import Flask
+import json
+pets = json.load(open('pets.json'))
+print(pets)
 
 def create_app():
     app = Flask(__name__)
+    app.config['TEMPLATES_AUTO_RELOAD'] = True
 
     @app.route('/')
     def hello():
         return 'Hello, PetFax!'
 
-    from . import pet
-    app.register_blueprint(pet.bp)
+    from .pet import bp as pet_bp
+    app.register_blueprint(pet_bp)
+
+    from .facts import bp as facts_bp
+    app.register_blueprint(facts_bp)
     
     return app
--- petfax/pet.py
+++ petfax/pet.py
@@ -7,3 +7,12 @@ bp = Blueprint('pet', __name__, url_prefix="/pets")
 @bp.route('/')
 def index():
     return render_template('index.html', pets=pets)
+
+
+@bp.route('/<int:pet_id>')
+def show(pet_id): # pet_id == 1
+    for num in range(len(pets)): # for num == 0 in [0, 1, 2]:
+        if pets[num]["pet_id"] == pet_id: # 1 == 1 == true
+            return render_template('show.html', pet=pets[num])
+    return "404 not found"
+
